f812bf845a9d79307e086e3951d26877a8b3a497
hunglegion <nvhk53c@gmail.com>
2024-08-05 21:37:58 +0700

    #26 @RequestBody

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,6 +1,8 @@
 package vn.hoidanit.jobhunter.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
@@ -14,13 +16,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/create")
-    public String createNewUser() {
-        User user = new User();
-        user.setEmail("hung@gmail.com");
-        user.setName("Eric");
-        user.setPassword("123456");
-        this.userService.handlerCreateUser(user);
-        return "create user";
+    // @GetMapping("/user/create")
+    @PostMapping("/user/create")
+    public User createNewUser(@RequestBody User postmanUser) {
+        return this.userService.handlerCreateUser(postmanUser);
     }
 }
