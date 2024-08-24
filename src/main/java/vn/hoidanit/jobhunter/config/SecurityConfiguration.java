5d716f2f49482fdad0d9dbf4819b75c34de192bd
Viet Hung Nguyen <nvhk53c@gmail.com>
2024-08-24 11:40:12 +0700

    #99 read file from path

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -45,7 +45,8 @@ public class SecurityConfiguration {
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
                         authz -> authz
-                                .requestMatchers("/", "/login", "/api/v1/auth/login", "/api/v1/auth/refresh")
+                                .requestMatchers("/", "/login", "/api/v1/auth/login", "/api/v1/auth/refresh",
+                                        "/storage/**")
                                 .permitAll()
                                 .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
