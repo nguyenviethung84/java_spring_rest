115f2e77ff478e5903ae67b0352acfc03d56cd04
Viet Hung Nguyen <nvhk53c@gmail.com>
2024-08-30 22:32:18 +0700

    #127 send email với template html 3

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -7,15 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.service.EmailService;
+import vn.hoidanit.jobhunter.service.SubscriberService;
 import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
 
 @RestController
 @RequestMapping("/api/v1")
 public class EmailController {
     private final EmailService emailService;
+    private final SubscriberService subscriberService;
 
-    public EmailController(EmailService emailService) {
+    public EmailController(EmailService emailService, SubscriberService subscriberService) {
         this.emailService = emailService;
+        this.subscriberService = subscriberService;
     }
 
     @GetMapping("/email")
@@ -23,7 +26,9 @@ public class EmailController {
     public ResponseEntity<String> sendSimpleEmail() {
         try {
             // this.emailService.sendSimpleEmail();
-            this.emailService.sendEmailFromTemplateSync("nvhk53c@gmail.com", "test send email", "job");
+            // this.emailService.sendEmailFromTemplateSync("nvhk53c@gmail.com", "test send
+            // email", "job");
+            this.subscriberService.sendSubscribersEmailJobs();
             return ResponseEntity.ok("Email sent successfully");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
