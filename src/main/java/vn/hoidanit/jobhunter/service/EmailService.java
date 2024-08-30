c9f6c4faf2b55ded0a0e501a7e2ea39ebd31edcf
Viet Hung Nguyen <nvhk53c@gmail.com>
2024-08-30 22:37:13 +0700

    update

--- src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
@@ -1,8 +1,6 @@
 package vn.hoidanit.jobhunter.service;
 
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
@@ -15,22 +13,17 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import vn.hoidanit.jobhunter.domain.Job;
-import vn.hoidanit.jobhunter.repository.JobRepository;
 
 @Service
 public class EmailService {
     private final MailSender mailSender;
     private final JavaMailSender javaMailSender;
     private final SpringTemplateEngine templateEngine;
-    private final JobRepository jobRepository;
 
-    public EmailService(MailSender mailSender, JavaMailSender javaMailSender, SpringTemplateEngine templateEngine,
-            JobRepository jobRepository) {
+    public EmailService(MailSender mailSender, JavaMailSender javaMailSender, SpringTemplateEngine templateEngine) {
         this.mailSender = mailSender;
         this.javaMailSender = javaMailSender;
         this.templateEngine = templateEngine;
-        this.jobRepository = jobRepository;
     }
 
     public void sendSimpleEmail() {
